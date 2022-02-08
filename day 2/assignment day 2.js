454d83921f60684e7a197b8d1ea5da2a0b119715
ykaransingha <99267351+ykaransingha@users.noreply.github.com>
2022-02-08 20:34:41 +0530

    Assignment Submitted Day 2(Y. Karan Singha)

---
+++ day 2/assignment day 2.js
@@ -0,0 +1,11 @@
+//DAY 2 ASSIGNMENT OF Y.KARAN SINGHA
+//CREATE TWO VARIABLES AND USE ARITHMETIC, ASSIGNMENT, COMPARISION, IDENTITY, CONDITIONAL AND UNARY OPERATORS
+
+var num1 = 20, num2 = 10;
+console.log(num1 + num2);
+console.log(num1 -= num2);
+console.log(num1 == num2);
+console.log(num1 === num2);
+console.log(num2 < num1 && num1 != num2);
+var x = ++num1;
+console.log(x);
